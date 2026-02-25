8286188b20c7f572d08a4b7d47c6e019d0816e7a
Your Name <user@example.com>
2026-02-25 20:53:46 +0530

    Fix outputs.tf - correct module attribute names

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,19 +8,9 @@ output "vpc_cidr" {
   value       = module.vpc.vpc_cidr_block
 }
 
-output "vpc_name" {
-  description = "The VPC name tag"
-  value       = module.vpc.vpc_tags["Name"]
-}
-
 output "public_subnet_ids" {
   description = "IDs of public subnets"
   value       = module.vpc.public_subnets
-}
-
-output "public_subnet_arns" {
-  description = "ARNs of public subnets"
-  value       = module.vpc.public_subnet_arns
 }
 
 output "private_subnet_ids" {
@@ -28,19 +18,9 @@ output "private_subnet_ids" {
   value       = module.vpc.private_subnets
 }
 
-output "private_subnet_arns" {
-  description = "ARNs of private subnets"
-  value       = module.vpc.private_subnet_arns
-}
-
 output "data_subnet_ids" {
   description = "IDs of data subnets"
   value       = module.vpc.database_subnets
-}
-
-output "data_subnet_arns" {
-  description = "ARNs of data subnets"
-  value       = module.vpc.database_subnet_arns
 }
 
 output "internet_gateway_id" {
@@ -50,14 +30,7 @@ output "internet_gateway_id" {
 
 output "nat_gateway_ids" {
   description = "NAT Gateway IDs"
-  value       = [for gw in module.vpc.nat_gateway_ids : gw]
-  sensitive   = true
-}
-
-output "nat_gateway_ips" {
-  description = "NAT Gateway Elastic IPs"
-  value       = [for gw in module.vpc.nat_public_ips : gw]
-  sensitive   = true
+  value       = module.vpc.natgw_ids
 }
 
 output "security_group_alb" {
@@ -75,6 +48,11 @@ output "security_group_db" {
   value       = aws_security_group.db.id
 }
 
+output "security_group_bastion" {
+  description = "Bastion Security Group ID"
+  value       = aws_security_group.bastion.id
+}
+
 output "public_route_table_id" {
   description = "Public Route Table ID"
   value       = module.vpc.public_route_table_ids[0]
@@ -83,11 +61,6 @@ output "public_route_table_id" {
 output "private_route_table_id" {
   description = "Private Route Table ID"
   value       = module.vpc.private_route_table_ids[0]
-}
-
-output "data_route_table_id" {
-  description = "Data Route Table ID"
-  value       = module.vpc.database_route_table_ids[0]
 }
 
 output "azs" {
